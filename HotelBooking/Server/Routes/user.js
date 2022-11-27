a2e9ff942286f68f05b4f5e903f9f4c70c149fbc
rishit128 <parikhrishit@gmail.com>
2022-11-27 23:48:50 +0530

    Hotel
    Hotel

---
+++ HotelBooking/Server/Routes/user.js
@@ -0,0 +1,5 @@
+import express from "express";
+import { signin } from "../controllers/user.js";
+const router = express.Router();
+router.post("/signin", signin);
+export default router;
---
+++ HotelBooking/Server/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import "dotenv/config";
+import userRouter from "./routes/user.js";
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use("/user", userRouter);
+const uri = process.env.ATLAS_URI;
+const PORT = process.env.PORT || 5000;
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
